3d9cd0eaad70756891f1aeb11ebb22253098fd39
brandon <brandon@bethel-churchs-imac.local>
2010-05-20 10:19:51 -0700

    Adding GA on public pages

--- application/views/errors/404.php
+++ application/views/errors/404.php
@@ -3,16 +3,4 @@
 <h2>Are you trying to break something?  <br />Or did I break something?</h2>
 </div>
 
-<script type="text/javascript">
-
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-16528426-1']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-</script>
+<?=View::factory('shared/analytics')?>
