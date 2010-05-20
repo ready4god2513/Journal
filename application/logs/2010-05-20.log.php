d774bd45a8264aee126767d405bd9fa58a8b9c1a
brandon <brandon@bethel-churchs-imac.local>
2010-05-20 10:15:20 -0700

    Adding GA on public pages

---
+++ application/logs/2010-05-20.log.php
@@ -0,0 +1,10 @@
+<?php defined('SYSPATH') or die('No direct script access.'); ?>
+
+2010-05-20 08:09:18 -07:00 --- error: Uncaught Kohana_Database_Exception: There was an SQL error: No database selected - SELECT *
+FROM (`sessions`)
+WHERE `session_id` = '434e2bc1d5ed3dad1b666cc23b0b555d'
+LIMIT 0, 1 in file system/libraries/drivers/Database/Mysql.php on line 371
+2010-05-20 08:09:19 -07:00 --- error: Uncaught Kohana_Database_Exception: There was an SQL error: No database selected - SELECT *
+FROM (`sessions`)
+WHERE `session_id` = '434e2bc1d5ed3dad1b666cc23b0b555d'
+LIMIT 0, 1 in file system/libraries/drivers/Database/Mysql.php on line 371
