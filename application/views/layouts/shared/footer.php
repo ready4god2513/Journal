75a0c2cf6301a510174a39b7283cfac7e544a8a4
brandon <brandon@bethel-churchs-imac.local>
2010-07-15 14:30:51 -0700

    Layout

--- application/views/layouts/shared/footer.php
+++ application/views/layouts/shared/footer.php
@@ -1,23 +1,25 @@
 	</div>
 </div>
 
-<?php if(frache::start('footer_navigation')): ?>
-	<div class="container" id="footer_navigation">
-		<?=html::anchor('', 'Home')?>
-		<?php foreach(statics::navigation() as $page): ?>
-			<?=html::anchor($page->show_path(), $page->title)?>
-		<?php endforeach; ?>
-		<?=html::anchor('blog', 'Blog')?>
-		<?=html::anchor('http://twitter.com/tojournal', 'Twitter')?>
-	</div>
-<?php frache::stop(); endif; ?>
-
-<?php if(layout::output('pre_footer', false)): ?>
-	<div class="container" id="pre_footer">
-		<?=layout::output('pre_footer')?>
-	</div>
-<?php endif; ?>
-
-<?=layout::output('footer')?>
+<div id="full_footer">
+	<?php if(frache::start('footer_navigation')): ?>
+		<div class="container" id="footer_navigation">
+			<?=html::anchor('', 'Home')?>
+			<?php foreach(statics::navigation() as $page): ?>
+				<?=html::anchor($page->show_path(), $page->title)?>
+			<?php endforeach; ?>
+			<?=html::anchor('blog', 'Blog')?>
+			<?=html::anchor('http://twitter.com/tojournal', 'Twitter')?>
+		</div>
+	<?php frache::stop(); endif; ?>
+	
+	<?php if(layout::output('pre_footer', false)): ?>
+		<div class="container" id="pre_footer">
+			<?=layout::output('pre_footer')?>
+		</div>
+	<?php endif; ?>
+	
+	<?=layout::output('footer')?>
+</div>
 </body>
 </html>
